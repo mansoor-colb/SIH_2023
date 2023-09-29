3a60e01b95339331ac5e96f64c74405adc160b2d
Divyanshi Gupta <1dt20cs043@dsatm.edu.in>
2023-09-29 21:08:24 +0530

    Changes

--- public/assets/customjs/docgen.js
+++ public/assets/customjs/docgen.js
@@ -10,11 +10,25 @@ $(document).ready(function(){
             dataType:"json",
             data:{d:val},       
             beforeSend:function(){
-                $("#detailsform").html(`Generating....`)
+                $("#detailsform").css({"justify-content": "center"})
+                $("#detailsform").html(`
+                <div class="content">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+            </div>`)
 
+            $("#generate").html("Generating.....")
+            $('#generate').prop('disabled', true);
+            $("#view").slideUp();
             },
            success: function (res) {
             if(res.data==1){
+               
+                $("#detailsform").css({"justify-content": "unset"})
+                $('#generate').prop('disabled', false);
+                $("#generate").html("Generate")
                 $("#detailsform").html(``)
              
                 let r=res.res.replace(/[\[\]]/g, '');
@@ -41,15 +55,15 @@ $(document).ready(function(){
             
         }
     });
+    
 
 
-
-
-    $('#details').submit(function (e) {
+    const form =   $('#details');
+  form.submit(function (e) {
      alert(2)
          e.preventDefault(); // Prevent the default form submission
  
-         const formData = new FormData(this);
+        //  const formData = new FormData(this);
        
         //  if (
         //      formData.get('val-album').trim() === '' ||
@@ -59,21 +73,52 @@ $(document).ready(function(){
         //      swal("Invalid ",'Please fill in all fields',"error");
         //      return;
         //  }
- 
+        const formData = form.serializeArray();
+        const formValues = {};
+  
+        $.each(formData, function(index, field) {
+          formValues[field.name] = field.value;
+          
+        });
+        for (const key in formValues) {
+            if (formValues.hasOwnProperty(key)) {
+              console.log(`Key: ${key}, Value: ${formValues[key]}`);
+            }
+          }
+        // console.log(formValues)
          $.ajax({
              type: 'POST',
              url: `http://localhost:1223/detailinsert`, // The URL to your server-side route
-             data: formData,
-             processData: false,
-             contentType: false,
+             data:{type:val,dat:JSON.stringify(formValues)},
+             beforeSend:function(){
+                $("#doccontent").css({"display":"flex","justify-content": "center"})
+                $("#doccontent").html(`
+                <div class="content">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+            </div>
+                
+                `)
+
+             },
+          dataType:"json",
              success: function (res) {
+                $("#view").slideUp();
                  // Handle the success response from the server
-                 if(res.status==200){
+                 if(res.data==1){
+                    $("#view").slideDown();
+                   
+                    $("#doccontent").css({"justify-content": "unset"})
+                    $("#doccontent").html('')
+                    $("#doccontent").html(`<pre contenteditable="true">${res.res.result}</pre>`)
                   
                  }
                  else if(res.status==500){
                    
- 
+                    
+
                  }
                  console.log(res);
               
